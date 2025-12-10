852da692526ec63505ab1e597921168768b443d3
Dobrosi Andras <dobrosi@gmail.com>
2025-12-10 08:28:58 +0100

    daily

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-curl -L -o channels.xml "https://kizman.net/guide/guide.xml"
+curl --insecure -L -o channels.xml "https://kizman.net/guide/guide.xml"
 unset spring_config_location
 java -jar jxmltv-0.0.1-SNAPSHOT.jar
 git add .
